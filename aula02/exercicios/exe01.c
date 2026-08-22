0cab9da566c940b601048fcba4d4ac0a2dcf0d9a
Marcos Arambasic <marcos.arambasic.silva@gmail.com>
2026-08-22 01:37:04 +0000

    Exercicio 1 - a fazer

---
+++ aula02/exercicios/exe01.c
@@ -0,0 +1,15 @@
+/*
+1. Implemente um programa utilizando a chamada de função wait() 
+que espere todos os n processos filhos terminarem, e
+ imprima na tela o PID e o código de retorno de cada um. 
+ O processo pai não deve guardar os PIDs dos filhos criados, 
+ e deve mostrar, no mínimo, a seguinte saída:
+
+``` 
+No processo pai: processo filho: 64464, status de saída do filho: 1
+No processo pai: processo filho: 64463, status de saída do filho: 3
+No processo pai: processo filho: 64462, status de saída do filho: 8
+No processo pai: processo filho: 64465, status de saída do filho: 10
+```
+*/
+
